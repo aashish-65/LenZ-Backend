6b35ace17b0c2451ee2af90e86fb3efd7bad051b
aashish-65 <aashishprasad65@gmail.com>
2024-12-09 02:11:14 +0530

    Create test api

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.log(err));
 
 // Routes
-app.use('/api/auth', authRoutes); // Signup/Login
+app.use('/api/auth', authRoutes);
+app.get('/api/test', (req, res) => {
+  res.json({ notification: "false" }); // Send JSON response
+});
 // app.use('/api/orders', require('./routes/orders')); // Frame orders
 
 const PORT = process.env.PORT || 5000;
